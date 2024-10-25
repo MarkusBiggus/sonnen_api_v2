86c14206887dc76d0963e83ca0fb658f5854c05a
Mark <6531552+MarkusBiggus@users.noreply.github.com>
2024-10-25 10:08:52 +1000

    Refactor HTTP gets to be async (#11)
    * update dev with Katamave fork changes (#6)
    
    * async updates and logging
    
    * async updates and logging
    
    * manual merge of async and other changes from Katamave main
    
    * wip
    
    ---------
    
    Co-authored-by: Vaclav <katamave@gmail.com>
    
    * optional return types for undefined values
    
    * all tests pass  after manual merge
    
    * module name conform to style guide
    
    * wrapped functions
    
    * wrapped functions imported correctly & tested
    
    * detect and return error condition
    
    * skiptest on API fail
    
    * refactored HTTP gets to be async - batterie tests pass
    
    ---------
    
    Co-authored-by: Vaclav <katamave@gmail.com>

--- sonnen_api_v2/wrapped.py
+++ sonnen_api_v2/wrapped.py
@@ -1,5 +1,8 @@
 from typing import Union
 
+import aiohttp
+import asyncio
+
 __all__ = [
     "set_request_connect_timeouts",
     "get_request_connect_timeouts",
@@ -7,14 +10,10 @@ __all__ = [
     "get_configurations",
     "get_status"
     ]
-#class wrapped:
-#    """ sonnenbatterie_api_v2 wrapper support functions """
-
-#    def __init__(self) -> None:
-#        return
 
 def set_request_connect_timeouts(self, request_timeouts: tuple[int, int]):
     self.request_timeouts = request_timeouts
+    self.client_timeouts = aiohttp.ClientTimeout(connect=request_timeouts[0], sock_read=request_timeouts[1])
 
 def get_request_connect_timeouts(self) -> tuple[int, int]:
     return self.request_timeouts
@@ -25,63 +24,99 @@ def get_latest_data(self)-> Union[str, bool]:
         Returns:
             json response
     """
-    success = self.fetch_latest_details()
-    if not success:
-        return success
+    async def _get_latest_data(self):
+        await self.fetch_latest_details()
 
-    return self._latest_details_data
+    event_loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(event_loop)
+    try:
+        event_loop.run_until_complete(_get_latest_data(self))
+    finally:
+        event_loop.close()
+
+    return self._latest_details_data if self._latest_details_data is not None else False
 
 def get_configurations(self)-> Union[str, bool]:
     """Configuration details for sonnenbatterie wrapper
         Returns:
             json response
     """
-    success = self.fetch_configurations()
-    if not success:
-        return success
+    async def _get_configurations(self):
+        await self.fetch_configurations()
 
-    return self._configurations_data
+    event_loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(event_loop)
+    try:
+        event_loop.run_until_complete(_get_configurations(self))
+    finally:
+        event_loop.close()
+
+    return self._configurations_data if self._configurations_data is not None else False
 
 def get_status(self)-> Union[str, bool]:
     """Status details for sonnenbatterie wrapper
         Returns:
             json response
     """
-    success = self.fetch_status()
-    if not success:
-        return success
+    async def _get_status(self):
+        await self.fetch_status()
 
-    return self._status_data
+    event_loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(event_loop)
+    try:
+        event_loop.run_until_complete(_get_status(self))
+    finally:
+        event_loop.close()
+
+    return self._status_data if self._status_data is not None else False
 
 def get_powermeter(self)-> Union[str, bool]:
     """powermeter details for sonnenbatterie wrapper
         Returns:
             json response
     """
-    success = self.fetch_powermeter()
-    if not success:
-        return success
+    async def _get_powermeter(self):
+        await self.fetch_powermeter()
 
-    return self._powermeter_data
+    event_loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(event_loop)
+    try:
+        event_loop.run_until_complete(_get_powermeter(self))
+    finally:
+        event_loop.close()
+
+    return self._powermeter_data if self._powermeter_data is not None else False
 
 def get_battery(self)-> Union[str, bool]:
     """Battery status for sonnenbatterie wrapper
         Returns:
             json response
     """
-    success = self.fetch_battery_status()
-    if not success:
-        return success
+    async def _get_battery(self):
+        await self.fetch_battery_status()
 
-    return self._battery_status
+    event_loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(event_loop)
+    try:
+        event_loop.run_until_complete(_get_battery(self))
+    finally:
+        event_loop.close()
+
+    return self._battery_status if self._battery_status is not None else False
 
 def get_inverter(self)-> Union[str, bool]:
     """Inverter details for sonnenbatterie wrapper
         Returns:
             json response
     """
-    success = self.fetch_inverter_data()
-    if not success:
-        return success
+    async def _get_inverter(self):
+        await self.fetch_inverter_data()
 
-    return self._inverter_data
+    event_loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(event_loop)
+    try:
+        event_loop.run_until_complete(_get_inverter(self))
+    finally:
+        event_loop.close()
+
+    return self._inverter_data if self._inverter_data is not None else False

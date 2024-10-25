94dde756fac9bad9ff6f84ec10591a873cc6652a
Mark <6531552+MarkusBiggus@users.noreply.github.com>
2024-10-25 11:08:47 +1000

    get Inverter properly & test it (#12)
    * update dev with Katamave fork changes (#6)
    
    * async updates and logging
    
    * async updates and logging
    
    * manual merge of async and other changes from Katamave main
    
    * wip
    
    ---------
    
    Co-authored-by: Vaclav <katamave@gmail.com>
    
    * optional return types for undefined values
    
    * all tests pass  after manual merge
    
    * module name conform to style guide
    
    * wrapped functions
    
    * wrapped functions imported correctly & tested
    
    * detect and return error condition
    
    * skiptest on API fail
    
    * refactored HTTP gets to be async - batterie tests pass
    
    * get Inverter data properly & test it
    
    ---------
    
    Co-authored-by: Vaclav <katamave@gmail.com>

--- sonnen_api_v2/sonnen.py
+++ sonnen_api_v2/sonnen.py
@@ -13,7 +13,7 @@ import asyncio
 #import aiohttp_fast_zlib
 
 import logging
-import requests
+import json
 from .const import *
 
 def get_item(_type):
@@ -205,12 +205,16 @@ class Sonnen:
             success = await self.fetch_battery_status()
         if success:
             success = await self.fetch_powermeter()
+        if success:
+            success = await self.fetch_inverter_data()
+
         self.last_updated = datetime.datetime.now() if success else None
         return success
 
     def update(self) -> bool:
         event_loop = asyncio.new_event_loop()
         asyncio.set_event_loop(event_loop)
+
         try:
             event_loop.run_until_complete(self._update())
         finally:
@@ -288,8 +292,8 @@ class Sonnen:
 
     async def fetch_inverter_data(self) -> Optional[str]:
         self._inverter_data = None
-        self.inverter_api_endpoint = await self._async_fetch_api_endpoint(
-            self._inverter_data
+        self._inverter_data = await self._async_fetch_api_endpoint(
+            self.inverter_api_endpoint
         )
         return (self._inverter_data is not None)
 
@@ -879,6 +883,15 @@ class Sonnen:
         """
         print (f'{self._status_data[STATUS_TIMESTAMP]}')
         return  datetime.datetime.fromisoformat(self._status_data[STATUS_TIMESTAMP])
+
+    @property
+    @get_item(float)
+    def inverter_pac_total(self) -> float:
+        """Inverter PAC total"
+            Returns:
+                float
+        """
+        return  self._inverter_data[INVERTER_PAC_TOTAL]
 
     @property
     def validation_timestamp(self) -> datetime.datetime:
--- tests/test_batterie.py
+++ tests/test_batterie.py
@@ -39,100 +39,100 @@ class TestBatterie(unittest.TestCase):
     #    self.logger.info('Sonnen Live Batterie Test suite started.')
 
 
-        self.battery_live = Sonnen(API_READ_TOKEN, BATTERIE_HOST, LOGGER_NAME)  # Batterie online
+        self._battery = Sonnen(API_READ_TOKEN, BATTERIE_HOST, LOGGER_NAME)  # Batterie online
 
-        success = self.battery_live.update()
+        success = self._battery.update()
 #        self.assertTrue(success)
         if not success:
             self.skipTest('Failed to get battery data!')
 
     def test_configuration_de_software(self):
-        version = self.battery_live.configuration_de_software
-        cycles = self.battery_live.battery_cycle_count
+        version = self._battery.configuration_de_software
+        cycles = self._battery.battery_cycle_count
         print(f'Software Version: {version}   Battery Cycles: {cycles:,}')
         self.assertEqual(True, True)
 
     def test_configuration_em_usoc(self):
-        usoc = self.battery_live.configuration_em_usoc
+        usoc = self._battery.configuration_em_usoc
         self.assertEqual(usoc, BACKUP_BUFFER_USOC) # config BackupBuffer value
 
     def test_seconds_to_reserve(self):
-        seconds = self.battery_live.seconds_to_reserve
-        if self.battery_live.status_battery_discharging:
+        seconds = self._battery.seconds_to_reserve
+        if self._battery.status_battery_discharging:
             if seconds < 0:
                 print(f'Seconds since Reserve: {seconds:,}')
             else:
                 print(f'Seconds to Reserve: {seconds:,} (discharging)')
-            seconds = self.battery_live.seconds_to_empty
+            seconds = self._battery.seconds_to_empty
             print(f'Seconds to Empty: {seconds:,}')
         else:
             if seconds is None:
                 print(f'Seconds to reserve: Battery charging above reserve')
             else:
                 print(f'Seconds to Reserve: {seconds:,} (charging)')
-                usoc = self.battery_live.u_soc
-                if usoc == self.battery_live.configuration_em_usoc:
+                usoc = self._battery.u_soc
+                if usoc == self._battery.configuration_em_usoc:
                     print(f'Battery at Backup Reserve: {usoc}')
         self.assertEqual(True, True)
 
     def test_system_status(self):
-        state = self.battery_live.state_core_control_module
+        state = self._battery.state_core_control_module
         print (f'Core Control State: {state}')
-        status = self.battery_live.system_status
+        status = self._battery.system_status
         print (f'System Status: {status}')
-        status_timestamp = self.battery_live.system_status_timestamp
+        status_timestamp = self._battery.system_status_timestamp
         print (f'Status time: {status_timestamp.strftime("%d-%b-%Y %H:%M:%S")}')
-        validation_timestamp = self.battery_live.validation_timestamp
+        validation_timestamp = self._battery.validation_timestamp
         print (f'Validation time: {validation_timestamp.strftime("%d-%b-%Y %H:%M:%S")}')
         self.assertEqual(state, 'ongrid')
         self.assertEqual(status, 'OnGrid')
 
     def test_configuration_em_operatingmode(self):
-        OpMode = self.battery_live.configuration_em_operatingmode
-        OperatingMode = self.battery_live.str_em_operatingmode
-        DischargeAllowed = not(self.battery_live.status_discharge_not_allowed)
+        OpMode = self._battery.configuration_em_operatingmode
+        OperatingMode = self._battery.str_em_operatingmode
+        DischargeAllowed = not(self._battery.status_discharge_not_allowed)
         print (f'Operating Mode: "{OperatingMode}"  Discharge Allowed: {DischargeAllowed}')
         self.assertEqual(OpMode, OPERATING_MODE)
 
     def test_battery_remaining_capacity(self):
-        capacity = self.battery_live.battery_remaining_capacity
-        usable_capacity = self.battery_live.battery_usable_remaining_capacity
+        capacity = self._battery.battery_remaining_capacity
+        usable_capacity = self._battery.battery_usable_remaining_capacity
         print(f'RemainingAh capacity: {capacity:.2f}Ah  UsableAh: {usable_capacity:.2f}Ah')
         self.assertEqual(True, True)
 
     def test_battery_rsoc(self):
-        rsoc = self.battery_live.battery_rsoc
+        rsoc = self._battery.battery_rsoc
         print(f'Battery Relative State of Charge: {rsoc:.0f}%')
         self.assertEqual(True, True)
 
     def test_data_socs(self):
-        backup_buffer = self.battery_live.status_backup_buffer
-        usable_reserve = self.battery_live.backup_buffer_usable_capacity_wh
+        backup_buffer = self._battery.status_backup_buffer
+        usable_reserve = self._battery.backup_buffer_usable_capacity_wh
         print(f'Backup Buffer: {backup_buffer:2}%  Usable Reserve: {usable_reserve:,}Wh')
-        usoc = self.battery_live.u_soc
-        rsoc = self.battery_live.r_soc
-        seconds_to_reserve = self.battery_live.seconds_to_reserve
+        usoc = self._battery.u_soc
+        rsoc = self._battery.r_soc
+        seconds_to_reserve = self._battery.seconds_to_reserve
         print(f'Useable State of Charge: {usoc}%  Actual SOC: {rsoc}%  Seconds to reserve: {seconds_to_reserve}')
         if usoc > backup_buffer:
-            reserve_time = self.battery_live.backup_reserve_at
+            reserve_time = self._battery.backup_reserve_at
             if reserve_time is None:
                 print ('Battery above backup reserve, not discharging.')
             else:
                 print('Battery Discharge to Reserve at: ' + reserve_time.strftime('%d-%b-%Y %H:%M:%S'))
         else:
-            discharged_at = self.battery_live.fully_discharged_at
+            discharged_at = self._battery.fully_discharged_at
             print(f'Backup Fully Discharged at: ' + discharged_at.strftime('%d-%b-%Y %H:%M:%S'))
         self.assertEqual(True, True)
 
     def test_battery_charging(self):
-        charging = self.battery_live.status_battery_charging
-        discharging = self.battery_live.status_battery_discharging
+        charging = self._battery.status_battery_charging
+        discharging = self._battery.status_battery_discharging
         print(f'Battery: Charging: {charging} Discharging: {discharging}')
-        charging = self.battery_live.charging
-        discharging = self.battery_live.discharging
-        production = self.battery_live.production
-        consumption = self.battery_live.consumption
-        feedin = self.battery_live.status_grid_feed_in
+        charging = self._battery.charging
+        discharging = self._battery.discharging
+        production = self._battery.production
+        consumption = self._battery.consumption
+        feedin = self._battery.status_grid_feed_in
         if charging != 0:
             print(f'Battery Charging: {charging:,}W  Production: {production:,}W  Consumption: {consumption:,}W  Grid Export: {feedin:,}W ')
         else:
@@ -144,14 +144,14 @@ class TestBatterie(unittest.TestCase):
         self.assertEqual(True, True)
 
     def test_status_flows(self):
-        flows = self.battery_live.status_flows
+        flows = self._battery.status_flows
     #    pprint(getmembers(flows.items()))
         for key, value in flows.items():
             print(f'{key}: {value}')
         self.assertEqual(True, True)
 
     def test_status_grid_feed_in(self):
-        feedin = self.battery_live.status_grid_feed_in
+        feedin = self._battery.status_grid_feed_in
         if feedin < 0:
             print(f'Grid Import: {abs(feedin):,.0f}W')
         else:
@@ -159,12 +159,12 @@ class TestBatterie(unittest.TestCase):
         self.assertEqual(True, True)
 
     def test_last_time_full(self):
-        charged = self.battery_live.last_time_full
+        charged = self._battery.last_time_full
         print('Battery Last Full at: ' + charged.strftime('%d-%b-%Y %H:%M'))
         self.assertEqual(True, True)
 
     def test_fully_charged_at(self):
-        charged = self.battery_live.fully_charged_at
+        charged = self._battery.fully_charged_at
         if charged is None:
             next_full = '*not charging*'
         else:
@@ -173,36 +173,46 @@ class TestBatterie(unittest.TestCase):
         self.assertEqual(True, True)
 
     def test_powermeter(self):
-        kwh_consumed = self.battery_live.kwh_consumed
-        kwh_produced = self.battery_live.kwh_produced
+        kwh_consumed = self._battery.kwh_consumed
+        kwh_produced = self._battery.kwh_produced
         print(f'Power consumed: {kwh_consumed:,.1f}kWh  produced: {kwh_produced:,.1f}kWh')
         self.assertEqual(True, True)
 
+    def test_inverter(self):
+        pac_total = self._battery.inverter_pac_total
+        print (f'Inverter: {pac_total:,.2f}')
+        self.assertEqual(True, True)
+
     def test_remaining_capacity(self):
-        batteryCapacity = self.battery_live.battery_full_charge_capacity
-        batteryCapacityWh = self.battery_live.battery_full_charge_capacity_wh
-        capacity = self.battery_live.full_charge_capacity
-        remaining = self.battery_live.battery_remaining_capacity
-        usableRemaining = self.battery_live.battery_usable_remaining_capacity
-        remainingWh = self.battery_live.remaining_capacity_wh
+        batteryCapacity = self._battery.battery_full_charge_capacity
+        batteryCapacityWh = self._battery.battery_full_charge_capacity_wh
+        capacity = self._battery.full_charge_capacity
+        remaining = self._battery.battery_remaining_capacity
+        usableRemaining = self._battery.battery_usable_remaining_capacity
+        remainingWh = self._battery.remaining_capacity_wh
         print(f'CapacityWh(data): {capacity:,}Wh  RemainingWh(status): {remainingWh:,}Wh  RemainingAh(battery): {remaining:,.3f}Ah  UsableAh(battery): {usableRemaining:,.3f}Ah')
         print(f'CapacityAh(battery): {batteryCapacity:,.3f}Ah CapacityWh(battery): {batteryCapacityWh:,.3f}Wh')
         self.assertEqual(True, True)
 
     def test_eclipse_led(self):
-        eclipse_led = self.battery_live.ic_eclipse_led
+        eclipse_led = self._battery.ic_eclipse_led
         print('EclipseLEDs: ' + json.dumps(eclipse_led, indent=2))
-        self.assertEqual(0, 0)
+        self.assertEqual(True, True)
 
     def test_wrapped(self):
-        request_timeouts = self.battery_live.get_request_connect_timeouts()
+        request_timeouts = self._battery.get_request_connect_timeouts()
         print(f'request_timeouts: {request_timeouts}')
         self.assertEqual((20, 20), request_timeouts)
-        battery_status = self.battery_live.get_battery()
+        battery_status = self._battery.get_battery()
     #    print('battery_status: ' + json.dumps(battery_status, indent=2))
-        remaining = self.battery_live.battery_remaining_capacity
-        usableRemaining = self.battery_live.battery_usable_remaining_capacity
-        remainingWh = self.battery_live.remaining_capacity_wh
-        usableWh = floor(usableRemaining * self.battery_live.battery_system_dc_voltage)
+        inverter = self._battery.get_inverter()
+        self.assertNotEqual(inverter, False)
+        PAC = inverter.get("pac_total")
+        print (f'Inverter PAC: {PAC:.2f}W ')
+        remaining = self._battery.battery_remaining_capacity
+        usableRemaining = self._battery.battery_usable_remaining_capacity
+        remainingWh = self._battery.remaining_capacity_wh
+        usableWh = floor(usableRemaining * self._battery.battery_system_dc_voltage)
+
         print(f'RemainingAh: {remaining:,.3f}Ah  RemainingWh: {remainingWh:,}Wh')
         print(f'UsableAh: {usableRemaining:,.3f}Ah  UsableWh: {usableWh:,}Wh')

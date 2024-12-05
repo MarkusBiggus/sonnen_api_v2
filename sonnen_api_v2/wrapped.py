e697e8c8fbcddaa33c96e1b40d54ad4b704a4336
Mark <6531552+MarkusBiggus@users.noreply.github.com>
2024-12-05 13:24:02 +1000

    wip

--- sonnen_api_v2/wrapped.py
+++ sonnen_api_v2/wrapped.py
@@ -72,9 +72,9 @@ def get_status(self)-> Union[str, bool]:
         finally:
             event_loop.close()
     else:
-        self._status_data = await self.fetch_status()
-    #    self._status_data = asyncio.create_task(self.fetch_status())
- 
+    #    self._status_data = await self.fetch_status()
+        self._status_data = asyncio.create_task(self.fetch_status())
+        print(f'status_data: {self._status_data }[])
 #    event_loop = asyncio.new_event_loop()
 #    asyncio.set_event_loop(event_loop)
 #    try:

cc1dd6c50515942ac13256bb79578fd442765804
MarkusBiggus <6531552+MarkusBiggus@users.noreply.github.com>
2024-12-11 18:10:55 +1000

    wip: fix dependencies for ha component

--- sonnen_api_v2/wrapped.py
+++ sonnen_api_v2/wrapped.py
@@ -18,6 +18,7 @@ def get_request_connect_timeouts(self) -> tuple[int, int]:
 
 def get_status(self)-> Union[str, bool]:
     """Status details for sonnenbatterie wrapper
+        for Sync caller with Async fetch
         Returns:
             json response
     """
@@ -35,6 +36,7 @@ def get_status(self)-> Union[str, bool]:
 
 def sync_get_status(self)-> Union[str, bool]:
     """Status details for sonnenbatterie wrapper
+        for Sync caller with Sync fetch
         Returns:
             json response
     """
@@ -43,6 +45,7 @@ def sync_get_status(self)-> Union[str, bool]:
 
 def get_latest_data(self)-> Union[str, bool]:
     """Latest details for sonnenbatterie wrapper
+        for Sync caller with Async fetch
         Returns:
             json response
     """
@@ -62,6 +65,7 @@ def get_latest_data(self)-> Union[str, bool]:
 
 def sync_get_latest_data(self)-> Union[str, bool]:
     """Latest details for sonnenbatterie wrapper
+        for Sync caller with Sync fetch
         Returns:
             json response
     """
@@ -82,6 +86,7 @@ def _aug_latest_details(self):
 
 def get_configurations(self)-> Union[str, bool]:
     """Configuration details for sonnenbatterie wrapper
+        for Sync caller with Async fetch
         Returns:
             json response
     """
@@ -101,6 +106,7 @@ def get_configurations(self)-> Union[str, bool]:
 
 def sync_get_configurations(self)-> Union[str, bool]:
     """Configuration details for sonnenbatterie wrapper
+        for Sync caller with Sync fetch
         Returns:
             json response
     """
@@ -117,72 +123,10 @@ def _aug_configurations(self):
     if self._configurations_data is not None:
         self._configurations_data['DepthOfDischargeLimit'] = int((1 - BATTERY_UNUSABLE_RESERVE) * 100)
 
-def get_powermeter(self)-> Union[str, bool]:
-    """powermeter details for sonnenbatterie wrapper
-        Returns:
-            json response
-    """
-    async def _get_powermeter(self):
-        self._powermeter_data = await self.async_fetch_powermeter()
-
-    event_loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(event_loop)
-    try:
-        event_loop.run_until_complete(_get_powermeter(self))
-    finally:
-        event_loop.close()
-
-    if self._powermeter_data is not None:
-        self._powermeter_production = self._powermeter_data[0]
-        self._powermeter_consumption = self._powermeter_data[1]
-    else:
-        self._powermeter_production = None
-        self._powermeter_consumption = None
-
-    return self._powermeter_data if self._powermeter_data is not None else False
-
-def sync_get_powermeter(self)-> Union[str, bool]:
-    """powermeter details for sonnenbatterie wrapper
-        Returns:
-            json response
-    """
-    self._powermeter_data = self.fetch_powermeter()
-    if self._powermeter_data is not None:
-        self._powermeter_production = self._powermeter_data[0]
-        self._powermeter_consumption = self._powermeter_data[1]
-    else:
-        self._powermeter_production = None
-        self._powermeter_consumption = None
-    return self._powermeter_data if self._powermeter_data is not None else False
-
-def get_inverter(self)-> Union[str, bool]:
-    """Inverter details for sonnenbatterie wrapper
-        Returns:
-            json response
-    """
-    async def _get_inverter(self):
-        self._inverter_data = await self.async_fetch_inverter_data()
-
-    event_loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(event_loop)
-    try:
-        event_loop.run_until_complete(_get_inverter(self))
-    finally:
-        event_loop.close()
-
-    return self._inverter_data if self._inverter_data is not None else False
-
-def sync_get_inverter(self)-> Union[str, bool]:
-    """Inverter details for sonnenbatterie wrapper
-        Returns:
-            json response
-    """
-    self._inverter_data = self.fetch_inverter_data()
-    return self._inverter_data if self._inverter_data is not None else False
-
 def get_battery(self)-> Union[Dict, bool]:
     """Battery status for sonnenbatterie wrapper
         Fake V1 API data used by ha sonnenbatterie component
+        for Sync caller with Async fetch
         Returns:
             json response
     """
@@ -211,6 +155,7 @@ def get_battery(self)-> Union[Dict, bool]:
 def sync_get_battery(self)-> Union[Dict, bool]:
     """Battery status for sonnenbatterie wrapper
         Fake V1 API data used by ha sonnenbatterie component
+        for Sync caller with Sync fetch
         Returns:
             json response
     """
@@ -239,3 +184,70 @@ def _aug_battery(self):
     self._battery_status['remaining_capacity'] = self.battery_remaining_capacity_wh
     self._battery_status['remaining_capacity_usable'] = self.battery_usable_remaining_capacity_wh
     self._battery_status['backup_buffer_usable'] = self.backup_buffer_usable_capacity_wh
+
+def get_powermeter(self)-> Union[str, bool]:
+    """powermeter details for sonnenbatterie wrapper
+        for Sync caller with Async fetch
+        Returns:
+            json response
+    """
+    async def _get_powermeter(self):
+        self._powermeter_data = await self.async_fetch_powermeter()
+
+    event_loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(event_loop)
+    try:
+        event_loop.run_until_complete(_get_powermeter(self))
+    finally:
+        event_loop.close()
+
+    if self._powermeter_data is not None:
+        self._powermeter_production = self._powermeter_data[0]
+        self._powermeter_consumption = self._powermeter_data[1]
+    else:
+        self._powermeter_production = None
+        self._powermeter_consumption = None
+
+    return self._powermeter_data if self._powermeter_data is not None else False
+
+def sync_get_powermeter(self)-> Union[str, bool]:
+    """powermeter details for sonnenbatterie wrapper
+        for Sync caller with Sync fetch
+        Returns:
+            json response
+    """
+    self._powermeter_data = self.fetch_powermeter()
+    if self._powermeter_data is not None:
+        self._powermeter_production = self._powermeter_data[0]
+        self._powermeter_consumption = self._powermeter_data[1]
+    else:
+        self._powermeter_production = None
+        self._powermeter_consumption = None
+    return self._powermeter_data if self._powermeter_data is not None else False
+
+def get_inverter(self)-> Union[str, bool]:
+    """Inverter details for sonnenbatterie wrapper
+        for Sync caller with Async fetch
+        Returns:
+            json response
+    """
+    async def _get_inverter(self):
+        self._inverter_data = await self.async_fetch_inverter_data()
+
+    event_loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(event_loop)
+    try:
+        event_loop.run_until_complete(_get_inverter(self))
+    finally:
+        event_loop.close()
+
+    return self._inverter_data if self._inverter_data is not None else False
+
+def sync_get_inverter(self)-> Union[str, bool]:
+    """Inverter details for sonnenbatterie wrapper
+        for Sync caller with Sync fetch
+        Returns:
+            json response
+    """
+    self._inverter_data = self.fetch_inverter_data()
+    return self._inverter_data if self._inverter_data is not None else False

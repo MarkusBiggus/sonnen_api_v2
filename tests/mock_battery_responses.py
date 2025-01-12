c00b0d3230ee58a0ad1568bf8a964237de792305
MarkusBiggus <6531552+MarkusBiggus@users.noreply.github.com>
2025-01-12 18:56:52 +1000

    wip: refactor tests with HTTPResponse

--- tests/mock_battery_responses.py
+++ tests/mock_battery_responses.py
@@ -1,23 +1,24 @@
 """Mock battery configuration used as response to validate Auth"""
-
+from urllib3 import HTTPResponse
 import responses
 from . mock_sonnenbatterie_v2_charging import __mock_configurations
+import json
 
-def __battery_configurations_auth200(self, _method:str, _url:str, _body, _headers:str, _retries):
+def __battery_configurations_auth200(self, _method:str, _url:str, _body, _headers:str, _retries, **kwargs):
     """Mock response to validate Auth """
-    resp = responses.Response(
-        method=_method, #'GET',
-        url=_url, #(f'http://fakeHost:80/api/v2/configurations'),
-        json=__mock_configurations(),
+    resp = HTTPResponse(
+        request_method=_method, #'GET',
+        request_url=_url, #(f'http://fakeHost:80/api/v2/configurations'),
+        body=json.dumps(__mock_configurations()),
         status=200,
         headers=_headers,
     )
     #print(f'resp: {resp.body}')
     return resp
 
-def __battery_configurations_auth401(self, _method:str, _url:str, _body, _headers:str, _retries):
+def __battery_configurations_auth401(self, _method:str, _url:str, _body, _headers:str, _retries, **kwargs):
     """Mock response for invalid Auth """
-    resp = responses.Response(
+    resp = HTTPResponse(
         method=_method, #'GET',
         url=_url, #(f'http://fakeHost:80/api/v2/configurations'),
         status=401,
@@ -25,9 +26,9 @@ def __battery_configurations_auth401(self, _method:str, _url:str, _body, _header
     )
     return resp
 
-def __battery_configurations_auth500(self, _method:str, _url:str, _body, _headers:str, _retries):
+def __battery_configurations_auth500(self, _method:str, _url:str, _body, _headers:str, _retries, **kwargs):
     """Mock response API error """
-    resp = responses.Response(
+    resp = HTTPResponse(
         method=_method, #'GET',
         url=_url, #(f'http://fakeHost:80/api/v2/configurations'),
         status=500,

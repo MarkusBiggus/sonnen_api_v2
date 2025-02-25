cbc57d7d5a76ea4e591eb029814d1b2685123eb7
Mark <6531552+MarkusBiggus@users.noreply.github.com>
2025-02-25 16:55:36 +1000

    Update setup.py to match HASS requirements

--- setup.py
+++ setup.py
@@ -34,6 +34,9 @@ setup(
         'urllib3>=1.26.20,<2.0.0',
         'yarl~=1.18.3',
         "setuptools~=75.7.0",
+        "propcache~=0.2.1",
+        "attrs~=24.2.0",
+        "pytest-asyncio~=0.24.0",
     ],
     python_requires=">=3.12",
 )

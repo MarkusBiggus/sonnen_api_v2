dcc03ca60778f9b45f32acb47d7b5c7a9efbb62c
MarkusBiggus <6531552+MarkusBiggus@users.noreply.github.com>
2025-08-26 17:55:30 +1000

    wip

--- tests/battery_discharging_offgrid_asyncio.py
+++ tests/battery_discharging_offgrid_asyncio.py
@@ -1,5 +1,5 @@
 """Fixture to load batterie discharging below reserve responses
-    using all async methods called from async methods
+    using all async methods called from async methods (HA emulation)
 """
 import logging
 import pytest
@@ -9,20 +9,20 @@ from asyncmock import AsyncMock
 from sonnen_api_v2 import Batterie
 
 from . mock_sonnenbatterie_v2_charging import __mock_configurations, __mock_powermeter
-from . mock_sonnenbatterie_v2_discharging_reserve import __mock_status_discharging, __mock_latest_discharging, __mock_battery_discharging, __mock_inverter_discharging
+from . mock_sonnenbatterie_v2_discharging_offgrid import __mock_status_discharging, __mock_latest_discharging, __mock_battery_discharging, __mock_inverter_discharging
 
 
 LOGGER_NAME = "sonnenapiv2"
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-@pytest.fixture(name="battery_discharging_reserve")
+@pytest.fixture(name="battery_discharging_offgrid")
 @freeze_time("20-11-2023 17:00:59") # disharging reserve time
-async def fixture_battery_discharging_reserve(mocker) -> Batterie:
+async def fixture_battery_discharging_offgrid(mocker) -> Batterie:
     if LOGGER_NAME is not None:
         logging.basicConfig(filename=(f'/tests/logs/{LOGGER_NAME}.log'), level=logging.DEBUG)
         logger = logging.getLogger(LOGGER_NAME)
-        logger.info('Sonnen mock data battery_discharging_reserve_async test.')
+        logger.info('Sonnen mock data battery_discharging_offgrid_async test.')
 
     mocker.patch.object(Batterie, "async_fetch_status", AsyncMock(return_value=__mock_status_discharging()))
     mocker.patch.object(Batterie, "async_fetch_latest_details", AsyncMock(return_value=__mock_latest_discharging()))
@@ -31,9 +31,9 @@ async def fixture_battery_discharging_reserve(mocker) -> Batterie:
     mocker.patch.object(Batterie, "async_fetch_powermeter", AsyncMock(return_value=__mock_powermeter()))
     mocker.patch.object(Batterie, "async_fetch_inverter", AsyncMock(return_value=__mock_inverter_discharging()))
 
-    battery_discharging_reserve = Batterie('fakeToken', 'fakeHost')
+    battery_discharging_offgrid = Batterie('fakeToken', 'fakeHost')
 
-    success = await battery_discharging_reserve.async_update()
+    success = await battery_discharging_offgrid.async_update()
     assert success is not False
 
-    return battery_discharging_reserve
+    return battery_discharging_offgrid

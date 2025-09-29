4f1729f1959ecfd87b006521f80da3b0c6014c1e
MarkusBiggus <6531552+MarkusBiggus@users.noreply.github.com>
2025-09-30 08:02:02 +1000

    build #55

--- sonnen_api_v2/__init__.py
+++ sonnen_api_v2/__init__.py
@@ -13,7 +13,7 @@ __version__ = '0.5.15' # HA 2025.8
 # A build is each time dev work has passed its tests
 # and could be a release candidate.
 # Unrelated to version increases.
-__build__ = '54'
+__build__ = '55'
 
 __all__ = (
     "Batterie"
